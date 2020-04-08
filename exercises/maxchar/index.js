137168a7634afd813705efcc6f18cecbabdea374
Dossumov <UDossumov@poland.luxoft.com>
2020-04-08 22:38:47 +0200

    added maxchar sln

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,18 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charCountMap = {};
+  let max = 0;
+  let maxChar = '';
+  for (let char of str) {
+    charCountMap[char] = charCountMap[char] + 1 || 1;
+    if (charCountMap[char] > max) {
+      max = charCountMap[char];
+      maxChar = char;
+    }
+  }
+  return maxChar;
+}
 
 module.exports = maxChar;
