a38d653286f7f13d62e48448d8cf3e260e11c96b
Dossumov <UDossumov@poland.luxoft.com>
2020-04-15 13:42:07 +0200

    added matrix sln

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,53 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  let results = Array.from(Array(n), (_) => Array(n).fill(0));
+  let counter = 1;
+  let min_col = 0;
+  let max_col = n - 1;
+  let min_row = 0;
+  let max_row = n - 1;
+  let start_row = 0;
+  let end_row = n - 1;
+  let start_col = 0;
+  let end_col = n - 1;
+  while (counter <= n * n) {
+    min_col = start_col;
+    while (start_col <= end_col) {
+      results[start_row][start_col] = counter;
+      start_col++;
+      counter++;
+    }
+    start_col = min_col;
+    start_row++;
+    min_row = start_row;
+    while (start_row <= end_row) {
+      results[start_row][end_col] = counter;
+      start_row++;
+      counter++;
+    }
+    start_row = min_row;
+    end_col = end_col - 1;
+    max_col = end_col;
+    while (start_col <= end_col) {
+      results[end_row][end_col] = counter;
+      counter++;
+      end_col = end_col - 1;
+    }
+    end_col = max_col;
+    end_row = end_row - 1;
+    max_row = end_row;
+    while (start_row <= end_row) {
+      results[end_row][start_col] = counter;
+      counter++;
+      end_row = end_row - 1;
+    }
+    end_row = max_row;
+    start_col++;
+    min_col = start_col;
+  }
+  return results;
+}
 
 module.exports = matrix;
