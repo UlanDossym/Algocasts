e9eb54171cf75663aaefe8d4ddbd9787ca5823c8
Dossumov <UDossumov@poland.luxoft.com>
2020-04-17 21:30:09 +0200

    added levelwidth sln

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,21 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  if (!root) return null;
+  let counters = [0];
+  let queue = ['stopper', root];
+  while (queue.length > 1) {
+    let current = queue.pop();
+    if (current === 'stopper') {
+      counters.push(0);
+      queue.unshift('stopper');
+    } else {
+      queue.unshift(...current.children);
+      counters[counters.length - 1]++;
+    }
+  }
+  return counters;
+}
 
 module.exports = levelWidth;
