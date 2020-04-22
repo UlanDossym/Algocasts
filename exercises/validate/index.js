46ef3cf9bfb20d9cff501d2c3ff9a5508fea3852
Dossumov <UDossumov@poland.luxoft.com>
2020-04-22 12:06:33 +0200

    validate BST sln

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,16 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+function validate(node, min = null, max = null) {
+  if (min == null && max == null) {
+    min = this.data;
+    max = this.data;
+  }
+  if (min != null && node.data > min) return false;
+  if (max != null && node.data < max) return false;
+  if (node.left && !validate(node.left, node.left.data, max)) return false;
+  if (node.right && !validate(node.right, min, node.right.data)) return false;
+  return true;
+}
 
 module.exports = validate;
