4f54af9e1453a40a4483c6c4571caaa993f4c4cd
Dossumov <UDossumov@poland.luxoft.com>
2020-04-22 14:38:51 +0200

    sorting bubble, selection, merge added

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -2,19 +2,52 @@
 // Implement bubbleSort, selectionSort, and mergeSort
 
 function bubbleSort(arr) {
-
+  for (let i = 0; i < arr.length; i++)
+    for (let j = i; j < arr.length; j++)
+      if (arr[i] >= arr[j]) {
+        let b = arr[i];
+        arr[i] = arr[j];
+        arr[j] = b;
+      }
+  return arr;
 }
 
 function selectionSort(arr) {
-
+  let minIndex = 0;
+  for (let i = 0; i < arr.length; i++) {
+    minIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+    if (minIndex !== i) {
+      let temp = arr[i];
+      arr[i] = arr[minIndex];
+      arr[minIndex] = temp;
+    }
+  }
+  return arr;
 }
 
 function mergeSort(arr) {
-
+  if (arr.length === 1) return arr;
+  const center = Math.floor(arr.length / 2);
+  const left = arr.slice(0, center);
+  const right = arr.slice(center);
+  return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left, right) {
-
+  const results = [];
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      results.push(left.shift());
+    } else {
+      results.push(right.shift());
+    }
+  }
+  return [...results, ...left, ...right];
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
